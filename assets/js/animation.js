9479c2f37d3e1cd974dbdec973ee7c8947c9e6f8
ahmedmohamedcod <ahmedcodteng1@gmail.com>
2025-02-11 22:01:29 +0200

    commit

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,35 +1,35 @@
 
 
-  // إنشاء النجوم
-  function createStar() {
-    const star = document.createElement('div');
-    star.classList.add('star');
-    star.style.left = `${Math.random() * window.innerWidth}px`;
-    star.style.top = `${Math.random() * window.innerHeight}px`;
-    document.body.appendChild(star);
+  // // إنشاء النجوم
+  // function createStar() {
+  //   const star = document.createElement('div');
+  //   star.classList.add('star');
+  //   star.style.left = `${Math.random() * window.innerWidth}px`;
+  //   star.style.top = `${Math.random() * window.innerHeight}px`;
+  //   document.body.appendChild(star);
 
-    // إزالة النجوم بعد فترة
-    setTimeout(() => {
-      star.remove();
-    }, 2000);
-  }
+  //   // إزالة النجوم بعد فترة
+  //   setTimeout(() => {
+  //     star.remove();
+  //   }, 2000);
+  // }
 
-  // إنشاء اللمحات (sparkles)
-  function createSparkle() {
-    const sparkle = document.createElement('div');
-    sparkle.classList.add('sparkle');
-    sparkle.style.left = `${Math.random() * window.innerWidth}px`;
-    sparkle.style.top = `${Math.random() * window.innerHeight}px`;
-    document.body.appendChild(sparkle);
+  // // إنشاء اللمحات (sparkles)
+  // function createSparkle() {
+  //   const sparkle = document.createElement('div');
+  //   sparkle.classList.add('sparkle');
+  //   sparkle.style.left = `${Math.random() * window.innerWidth}px`;
+  //   sparkle.style.top = `${Math.random() * window.innerHeight}px`;
+  //   document.body.appendChild(sparkle);
 
-    // إزالة اللمحات بعد فترة
-    setTimeout(() => {
-      sparkle.remove();
-    }, 1500);
-  }
+  //   // إزالة اللمحات بعد فترة
+  //   setTimeout(() => {
+  //     sparkle.remove();
+  //   }, 1500);
+  // }
 
-  // إنشاء النجوم كل 100 مللي ثانية
-  setInterval(createStar, 100);
+  // // إنشاء النجوم كل 100 مللي ثانية
+  // setInterval(createStar, 100);
 
-  // إنشاء اللمحات كل 200 مللي ثانية
-  setInterval(createSparkle, 200);
+  // // إنشاء اللمحات كل 200 مللي ثانية
+  // setInterval(createSparkle, 200);
